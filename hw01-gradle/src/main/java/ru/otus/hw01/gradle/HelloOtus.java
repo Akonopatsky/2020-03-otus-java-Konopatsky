eceb209b2c83e98c208e34c2eef1c8ef8e9e6946
Akonopatsky <Akonopatsky@gmail.com>
2020-06-24 10:35:51 +0300

    mvp version 0.1

---
+++ hw01-gradle/src/main/java/ru/otus/hw01/gradle/HelloOtus.java
@@ -0,0 +1,15 @@
+package ru.otus.hw01.gradle;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println("HelloOtus ");
+        List<Integer> list1 = Lists.newArrayList(1, 2, 3, 4, 5);
+        System.out.println(list1);
+        System.out.println(Lists.reverse(list1));
+    }
+}
+
